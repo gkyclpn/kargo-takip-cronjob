e349c5725f3e6bb558570c80a8a981f3a1a1c247
gkyclpn <gokayculpan@hotmail.com>
2023-01-17 12:36:49 +0300

    last

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const { getAllMessages } = require('./jobs/kargo_takip_client.js');
 const schedule = require('node-schedule');
 
 
-schedule.scheduleJob('*/2 * * * * *', function () {
-    getEpsCount();
+schedule.scheduleJob('*/1 * * * * *', function () {
+    getAllMessages();
 });
 
 
